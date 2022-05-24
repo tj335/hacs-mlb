c4efbe5f6f6acb74d515fe5cf98196cba768f218
tj335 <105184198+tj335@users.noreply.github.com>
2022-05-24 11:45:04 -0400

    Added Team API

--- custom_components/mlb/const.py
+++ custom_components/mlb/const.py
@@ -1,5 +1,6 @@
 # API
-API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"
+API_SCOREBOARD_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"
+API_TEAM_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams/"
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
 
 # Config
@@ -14,7 +15,7 @@ DEFAULT_TIMEOUT = 180
 # Misc
 TEAM_ID = ""
 VERSION = "0.1"
-ISSUE_URL = "https://github.com/zacs/ha_nfl"
+ISSUE_URL = "https://github.com/tj335/hacs_mlb"
 DOMAIN = "mlb"
 PLATFORM = "sensor"
 ATTRIBUTION = "Data provided by ESPN"
